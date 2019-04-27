44897b378d91919b716b90ebf8ddc980d64688cd
Abhishek <abhishekg@wayforward.io>
2019-04-27 17:56:04 +0530

    added frontend default page shown public dirname

--- server.js
+++ server.js
@@ -6,10 +6,13 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var router = express.Router();
 var appRoutes = require('./app/routes/api')(router);
+var path = require('path');
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 // parse application/json
 app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+//app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api',appRoutes);   // slash api is used to distinguish between frontend and backend routes here
 
 mongoose.connect('mongodb://localhost:27017/assignment', {useNewUrlParser: true}, function(err){
@@ -21,11 +24,8 @@ mongoose.connect('mongodb://localhost:27017/assignment', {useNewUrlParser: true}
   }
 });
 
-app.get('/home',function(req,res){
-  console.log("hello from home");
-});
-app.get('/', function(req, res){
-  res.send('hello world');
+app.get('*', function(req, res){
+  res.sendFile(path.join(__dirname + '/public/app/views/index.html'));
 });
 app.listen(port,function(){
   console.log("the server is running");
